0f4b95dd7b9aaaf565aeb3a5a1275ba857112fe1
Malik <malek_noor40@hotmail.com>
2025-05-18 14:18:08 +0400

    malik part

--- calculator.py
+++ calculator.py
@@ -7,3 +7,10 @@ operation = None
 num1 = int(input("Enter the first number: "))
 num2 = int(input( "Enter the second number: "))
 operation = input("Choose an operation +, -, /, *" )
+
+if operation == "*":
+    result = num1 * num2
+elif operation == "/":
+    result = num1 / num2
+
+print (result)
