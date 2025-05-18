a15f3506646b82f2533d67fdce95831d1a0144d4
deena <deealbusaidi@gmail.com>
2025-05-18 12:16:43 +0400

    Declaration updated

--- calculator.py
+++ calculator.py
@@ -2,3 +2,8 @@ num1 = 0
 num2 = 0
 result = 0
 operation = None 
+
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input( "Enter the second number: "))
+operation = input("Choose an operation +, -, /, *" )
